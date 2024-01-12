4962dcc5b3b2896594ff242de083d90208a328fc
Jim Vomero <jim.vomero@fourkitchens.com>
2024-01-12 10:10:27 -0500

    changing scope to match github pages

--- whichwebchef/server.js
+++ whichwebchef/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(__dirname + '/public'));
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+// Middleware to "pretend" the subdirectory '/pwa' to match Github Pages.
+app.use((req, res, next) => {
+  // Remove the '/pwa' prefix from the URL before processing routes
+  req.url = req.url.replace(/^\/pwa/, '');
+  next();
 });
 
+// Serve static files from the 'public' directory.
+app.use(express.static(__dirname + '/public'));
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/pwa`);
+});
